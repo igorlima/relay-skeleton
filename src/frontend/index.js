9bba56eb4e03a88386b96f00289ef29bb2cc9de7
Igor Lima <ilima@C02Q93GKG8WP.local>
2015-10-22 16:11:58 -0700

    Print relay query

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -5,6 +5,8 @@ import Relay from 'react-relay';
 // purposefully calling Relay 'routes' roots (as in Query Root)
 import ExampleRoot from './roots/ExampleRoot';
 import Application from './containers/Application';
+import RelayQuery from 'react-relay/lib/RelayQuery.js';
+import RelayQueryRequest from 'react-relay/lib/RelayQueryRequest.js';
 
 class Root extends React.Component {
   render() {
@@ -15,6 +17,30 @@ class Root extends React.Component {
     );
   }
 }
+
+/**
+ * Walking through Relay code
+ */
+var relayRootContainer = new Relay.RootContainer( {
+	Component: Application,
+	route: new ExampleRoot()
+})
+
+var graphQLQuery = relayRootContainer.props.route.queries.example( relayRootContainer.props.Component ); // graphQLQuery instanceof GraphQLQuery
+
+var relayQueryRoot =  new RelayQuery.Root(
+	graphQLQuery,
+	relayRootContainer.props.route,
+	{})
+
+var relayQueryRequest = new RelayQueryRequest( relayQueryRoot )
+
+console.warn( 'Printing the query' )
+console.warn( relayQueryRequest.getQueryString() )
+
+/**
+ * The magic ends here
+ */
 
 ReactDOM.render(
   <Root />,
